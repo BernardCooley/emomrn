2aaf76977cd0d36613c6ddbee3af128879e9b9da
BernardCooley <bernardcooley@gmail.com>
2020-10-23 02:48:49 +0100

    get tracks correctly

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { tracks, artists } from '../Actions/index';
 import firestore from '@react-native-firebase/firestore';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import storage from '@react-native-firebase/storage';
 
 import TracksScreen from '../screens/Tracks';
@@ -12,27 +12,48 @@ import ArtistsScreen from '../screens/Artists';
 const ExploreScreen = ({ navigation }) => {
     const dispatch = useDispatch();
     const ExploreTab = createMaterialTopTabNavigator();
+    const allTracks = useSelector(state => state.tracks);
+    const [tracksTemp, setTracksTemp] = useState([]);
 
     useEffect(() => {
         firestore().collection('tracks').onSnapshot(getTracks, onError);
         firestore().collection('users').onSnapshot(getArtists, onError);
     }, []);
 
-    const getTracks = QuerySnapshot => {
-        allTracks = [];
-        QuerySnapshot.docs.map(async data => {
+    useEffect(() => {
+        getTrackImages();
+        console.log('+++++++++++++++++++++++')
+    }, [tracksTemp]);
+
+    const getTracks = async QuerySnapshot => {
+        setTracksTemp(QuerySnapshot.docs.map(data => {
             const trackData = data.data();
-            await storage().ref(`trackImages/${trackData.id}.jpg`).getDownloadURL().then(url => {
-                trackData['trackImage'] = url;
-                dispatch(tracks([...allTracks, trackData]));
-            });
-        });
+            return trackData;
+        }));
+    }
+
+    const getTrackImages = () => {
+        const tr = [];
+
+        tracksTemp.map(async (track, index) => {
+            await storage().ref(`trackImages/${track.id}.jpg`).getDownloadURL().then(url => {
+                track['trackImage'] = url;
+                tr.push(track);
+            }).catch(error => {
+                console.log('GET TRACK IMAGE =========>', error)
+            })
+            if (index === tracksTemp.length - 1) {
+                dispatch(tracks(tr));
+            }
+        })
+
+        
     }
 
     const getArtists = QuerySnapshot => {
         dispatch(artists(QuerySnapshot.docs.map(data => {
             const artistData = data.data();
-            artistData['trackAmount'] = allTracks.filter(track => track.artistId === artistData.userId).length;
+            artistData['trackAmount'] = allTracks.length > 0 ? allTracks.filter(track => track.artistId === artistData.userId).length : 0;
             return artistData;
         })));
     }
